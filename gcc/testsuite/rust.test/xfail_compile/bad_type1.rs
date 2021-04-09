90811368118edca28cef9b5fc58bb9a4f12e0cfc
lrh2000 <lrh2000@pku.edu.cn>
2021-04-09 23:43:10 +0800

    Update tests

--- gcc/testsuite/rust.test/xfail_compile/bad_type1.rs
+++ gcc/testsuite/rust.test/xfail_compile/bad_type1.rs
@@ -1,4 +1,10 @@
-fn main() {
+fn foo() -> i32 { // { dg-error "expected .i32. got .bool." }
     let logical: bool = 123; // { dg-error "expected .bool. got .<integer>." }
+    // { dg-error "failure in setting up let stmt type" "" { target { *-*-* } } .-1 }
+    logical
+}
+
+fn main() {
+    let a: bool = foo(); // { dg-error "expected .bool. got .i32." }
     // { dg-error "failure in setting up let stmt type" "" { target { *-*-* } } .-1 }
 }
